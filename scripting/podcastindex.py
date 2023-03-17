a1712b817e6bc7bf22df113b6bdbed395d8eca27
Nikola Kretschmer <nikokre@gmail.com>
2023-03-17 16:56:07 +0100

    addded env

--- scripting/podcastindex.py
+++ scripting/podcastindex.py
@@ -1,19 +1,25 @@
-
-# setup some basic vars for the search api. 
-# for more information, see https://api.podcastindex.org/developer_docs
-import calendar
-from datetime import date
+from dotenv import dotenv_values
 import hashlib
 import json
-import sys
 import time
-import requests
+from datetime import date
 
+import numpy
+import requests
 from mysql.connector import connect, Error
 
-api_key = 'WXRKPNKDNC3FKJNWLZYV'
-api_secret = 'MRZucBTzP9SJ3RWNuxGFcx^V4xYCwdh5ZFsku2EY'
+config = dotenv_values("../.env")
 
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+api_key = config["PCI_API_KEY"]
+api_secret = config["PCI_API_SECRET"]
 
 # we'll need the unix time
 epoch_time = int(time.time())
@@ -32,7 +38,6 @@ headers = {
 
 with open("podcast_data.json", "r") as file:
     data = json.load(file)
-
 
 """
     ideal data format:
@@ -55,9 +60,9 @@ with open("podcast_data.json", "r") as file:
 items = []
 
 for idx, val in enumerate(data):
-    if(idx < 10):
-        id = val["id"]
-        url = f"https://api.podcastindex.org/api/1.0/episodes/byfeedid?id={id}&max=40"
+    if idx < 5:
+        pod_id = val["id"]
+        url = f"https://api.podcastindex.org/api/1.0/episodes/byfeedid?id={pod_id}&max=30"
         r = requests.get(url, headers=headers)
         if r.status_code == 200:
             episodes = json.loads(r.text)
@@ -72,56 +77,63 @@ for idx, val in enumerate(data):
                 "image": val["image"],
                 "categories": val["categories"],
                 "drops": [x > 0 for x in weekdays],
-                "frequency": [x/sum(weekdays) for x in weekdays]
+                "frequency": [x / sum(weekdays) for x in weekdays]
             }
+            print(f"added {val['title']}")
             items.append(info)
 
 print("item length: ", len(items))
 
 try:
     with connect(
-        host="localhost",
-        user="root",
-        password="Naki1996ihg"
+            host=config["MYSQL_HOST"],
+            user=config["MYSQL_USER"],
+            password=config["MYSQL_PW"]
     ) as con:
         with con.cursor() as cursor:
             for item in items:
+                item["title"] = item["title"].replace("'", "").replace('"', '')
+                item["description"] = item["description"].replace("'", "").replace('"', '')
+
                 insert_podcasts = f'''
                     INSERT INTO podcastpill.podcasts
                     (podcast_id, title, description, image)
                     VALUES (
                         "{item["id"]}", 
                         "{item["title"]}", 
-                        "{item["description"]}", 
+                        "{remove_html_tags(item["description"])}", 
                         "{item["image"]}"
-                    )
+                    );
                 '''
 
+                cursor.execute(insert_podcasts)
 
-                cats = [""] * 4
-                for idx, element in enumerate(item["categories"].values()):
-                    cats[idx] = element
-                
+                cats = item["categories"].values()
+                params = [(x,) for x in cats]
+
                 insert_categories = f'''
                     INSERT INTO podcastpill.categories
-                    (podcast_id, category1, category2, category3, category4)
+                    (podcast_id, category)
                     VALUES (
                         "{item["id"]}", 
-                        "{cats[0]}", 
-                        "{cats[1]}", 
-                        "{cats[2]}", 
-                        "{cats[3]}"
-                    )
+                        %s
+                    );
                 '''
+
+                cursor.executemany(insert_categories, params)
 
                 drops = [0] * 7
                 for idx, element in enumerate(item["drops"]):
                     if element is True:
                         drops[idx] = 1
 
+                # Multiply with the frequency to get an accurate representation of
+                drops = numpy.multiply(drops, item["frequency"])
+                drops = drops.tolist()
+
                 insert_drops = f'''
                     INSERT INTO `podcastpill`.`drops`
-                    (`podcast_id`, `dropsMonday`, `dropsTuesday`,`dropsWednesday`,`dropsThursday`,`dropsFriday`,`dropsSaturday`,`dropsSunday`,`weights`)
+                    (`podcast_id`, `dropsMonday`, `dropsTuesday`,`dropsWednesday`,`dropsThursday`,`dropsFriday`,`dropsSaturday`,`dropsSunday`)
                     VALUES (
                         "{item["id"]}", 
                         "{drops[0]}", 
@@ -130,21 +142,14 @@ try:
                         "{drops[3]}",
                         "{drops[4]}",
                         "{drops[5]}",
-                        "{drops[6]}",
-                        "{item["frequency"]}"
+                        "{drops[6]}"
                     );                
                 '''
 
-                print(insert_categories)
-                print(insert_drops)
-
-
-                cursor.execute(insert_podcasts)
-                cursor.execute(insert_categories)
                 cursor.execute(insert_drops)
-                
+
                 con.commit()
-                sys.exit(0)
-        
+                print(f"pushed {item['title']} to DB")
+
 except Error as e:
     print(e)
