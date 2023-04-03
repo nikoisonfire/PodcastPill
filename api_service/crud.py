d042d4b34bca510bc34aab5dc337536a2a7835fc
Nikola Kretschmer <nikokre@gmail.com>
2023-04-03 14:31:34 +0200

    fixed empty list behavior

--- api_service/crud.py
+++ api_service/crud.py
@@ -1,7 +1,33 @@
-from sqlalchemy import func
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
 from api_service.models import Podcast, Category, Drops
+
+
+# Util
+def podcast_to_dict(row):
+    to_dict = row.Podcast.__dict__
+    # to_dict.pop("_sa_instance_state")
+
+    drop_dict = row.Podcast.drops.__dict__
+    drop_dict.pop("podcast_id")
+    # drop_dict.pop("_sa_instance_state")
+
+    categories = []
+    for cat in row.Podcast.category:
+        c_dict = cat.__dict__
+        categories.append(c_dict["category"])
+    # cat_dict.pop("_sa_instance_state")
+
+    to_dict["drops"] = drop_dict
+    to_dict["category"] = categories
+
+    return to_dict
+
+
+def get_all_podcasts(db: Session, limit: int = 100):
+    pods = db.query(Podcast).limit(limit).all()
+    return pods
 
 
 def get_podcast(db: Session, podcast_id: int):
@@ -9,16 +35,21 @@ def get_podcast(db: Session, podcast_id: int):
 
 
 def get_random_podcast(db: Session, weekday: str):
-    drops = db.query(Drops).filter(getattr(Drops, f"drops{weekday}") == 1).orderBy(func.random()).first()
-    return db.query(Podcast).filter(drops.podcast_id).first()
+    row = db.query(Drops).filter(getattr(Drops, f"drops{weekday}") > 0).order_by(func.random()).first()
+    return row.podcast.__dict__
 
 
 def get_podcasts_by_category(db: Session, category: str, limit: int = 10):
-    cat = db.query(Category).filter(Category.category == category).limit(limit).all()
-    return cat
+    # cat = db.query(Podcast, Drops.dropsMonday).join(Category, Category.podcast_id == Podcast.podcast_id).filter(
+    #   Category.category == category).filter(Drops.podcast_id == Podcast.podcast_id).limit(
+    #  limit).all()
+    statement = select(Podcast)
+    ex = db.execute(statement).first()
+
+    return podcast_to_dict(ex)
 
 
 def get_categories(db: Session):
-    # return db.query(Category.category).distinct().all()
-    # logging.info(db.connection())
-    return {}
+    query = db.query(Category.category).distinct().all()
+    cats = [x for (x,) in query]
+    return cats
--- api_service/models.py
+++ api_service/models.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from typing import List
 
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 
 from api_service.database import Base
 
@@ -10,55 +11,39 @@ from api_service.database import Base
 class Podcast(Base):
     __tablename__ = "podcasts"
 
-    id: int
-    title: str
-    description: str
-    image: str
+    podcast_id: int = Column(Integer, primary_key=True, index=True)
+    title: str = Column(String, index=True)
+    description: str = Column(String, index=True)
+    image: str = Column(String, index=True)
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    image = Column(String, index=True)
-
-    category = relationship("Category", back_populates="podcast")
-    drops = relationship("Drops", back_populates="podcast")
+    category: Mapped[List["Category"]] = relationship("Category", back_populates="podcast")
+    drops: Mapped["Drops"] = relationship("Drops", back_populates="podcast")
 
 
 @dataclass
 class Category(Base):
     __tablename__ = "categories"
 
-    id: int
+    cat_id: int
     podcast_id: int
     category: str
 
-    id = Column(Integer, primary_key=True, index=True)
-    podcast_id = Column(Integer, ForeignKey("podcasts.id"))
-    category = Column(String, index=True)
-    podcast = relationship("Podcast", back_populates="category")
+    cat_id: int = Column(Integer, primary_key=True, index=True)
+    podcast_id: int = Column(Integer, ForeignKey("podcasts.podcast_id"))
+    category: str = Column(String, index=True)
+    podcast: Mapped["Podcast"] = relationship("Podcast", back_populates="category")
 
 
 @dataclass
 class Drops(Base):
     __tablename__ = "drops"
-
-    id: int
-    podcast_id: int
-    dropsMonday: int
-    dropsTuesday: int
-    dropsWednesday: int
-    dropsThursday: int
-    dropsFriday: int
-    dropsSaturday: int
-    dropsSunday: int
-
-    id = Column(Integer, primary_key=True, index=True)
-    podcast_id = Column(Integer, ForeignKey("podcasts.id"))
-    dropsMonday = Column(Integer, index=True)
-    dropsTuesday = Column(Integer, index=True)
-    dropsWednesday = Column(Integer, index=True)
-    dropsThursday = Column(Integer, index=True)
-    dropsFriday = Column(Integer, index=True)
-    dropsSaturday = Column(Integer, index=True)
-    dropsSunday = Column(Integer, index=True)
-    podcast = relationship("Podcast", back_populates="drops")
+    
+    podcast_id: int = Column(Integer, ForeignKey("podcasts.podcast_id"), primary_key=True, index=True)
+    dropsMonday: float = Column(Integer, index=True)
+    dropsTuesday: float = Column(Integer, index=True)
+    dropsWednesday: float = Column(Integer, index=True)
+    dropsThursday: float = Column(Integer, index=True)
+    dropsFriday: float = Column(Integer, index=True)
+    dropsSaturday: float = Column(Integer, index=True)
+    dropsSunday: float = Column(Integer, index=True)
+    podcast: Mapped["Podcast"] = relationship("Podcast", back_populates="drops")
