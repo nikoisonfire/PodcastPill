939e963daeeb7782968ffc8f2e402de750faf74a
Nikola Kretschmer <nikokre@gmail.com>
2023-04-03 14:30:56 +0200

    added absolute path

--- api_service/database.py
+++ api_service/database.py
@@ -1,8 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///podpill.db"
+package_dir = os.path.abspath(os.path.dirname(__file__))
+db_dir = os.path.join(package_dir, 'podpill.db')
+# NEED 4 /'s to specify absolute for sqlalchemy!
+# ex: sqlite:////asdfaijegoij/aerga.db
+# NEED 3 /'s for relative paths
+# path has a / at the beginning so we have 3 here
+SQLALCHEMY_DATABASE_URL = ''.join(['sqlite:///', db_dir])
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True, echo_pool=True
