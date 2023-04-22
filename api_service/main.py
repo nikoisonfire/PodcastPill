3a883d7919e84006d44366c6f5776da77a3474d4
Nikola Kretschmer <nikokre@gmail.com>
2023-04-22 11:50:53 +0200

    exception handling 404

--- api_service/main.py
+++ api_service/main.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Annotated
 
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -40,37 +40,55 @@ def get_db():
 # return nothing on default
 @app.get("/")
 def root():
-    return {}
+    raise HTTPException(status_code=404, detail="Page not found")
 
 
 @app.get("/podcasts/{podcast_id}")
 def get_podcast(podcast_id: int, db: Session = Depends(get_db)):
-    return crud.get_podcast(db, podcast_id=podcast_id)
+    podcast = crud.get_podcast(db, podcast_id=podcast_id)
+    if podcast is None:
+        raise HTTPException(status_code=404, detail="Podcast not found")
+    return podcast
 
 
 @app.get("/podcasts")
 def get_podcast(limit: int = 10, db: Session = Depends(get_db)):
-    return crud.get_all_podcasts(db, limit=limit)
+    podcasts = crud.get_all_podcasts(db, limit=limit)
+    if podcasts is None:
+        raise HTTPException(status_code=404, detail="No podcasts found")
+    return podcasts
 
 
 @app.get("/random/{weekday}")
 def get_random_podcast(weekday: str, db: Session = Depends(get_db), limit: int = 10,
                        cat: Annotated[list[str] | None, Query()] = None):
-    return crud.get_random_podcast(db, weekday=weekday, categories=cat, limit=limit)
+    podcast = crud.get_random_podcast(db, weekday=weekday, categories=cat, limit=limit)
+    if podcast is None:
+        raise HTTPException(status_code=404, detail="No podcast found")
+    return podcast
 
 
 @app.get("/random-week")
 def get_random_week(db: Session = Depends(get_db), cat: Annotated[list[str] | None, Query()] = None):
-    return crud.get_random_week(db, categories=cat)
+    response = crud.get_random_week(db, categories=cat)
+    if response is None:
+        raise HTTPException(status_code=404, detail="No podcasts found")
+    return response
 
 
 @app.get("/podcasts/byCategory/{category}")
 def get_podcast_by_category(category: str, limit: int = 10, db: Session = Depends(get_db)):
-    return crud.get_podcasts_by_category(db, limit=limit, category=category)
+    podcasts = crud.get_podcasts_by_category(db, limit=limit, category=category)
+    if podcasts is None:
+        raise HTTPException(status_code=404, detail="No podcasts found")
+    return podcasts
 
 
 @app.get("/categories")
 def get_categories(db: Session = Depends(get_db)):
+    categories = crud.get_categories(db)
+    if categories is None:
+        raise HTTPException(status_code=404, detail="No categories found")
     return {
-        "categories": crud.get_categories(db)
+        "categories": categories
     }
